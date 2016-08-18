9b4e311b64f6d356f628e286a6f287aff13989eb
John Wright <johngeorge.wright@gmail.com>
2016-08-18 10:43:59 +0100

    Holiding page

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'standard',
+  plugins: [
+    'standard',
+    'promise'
+  ],
+  rules: {
+    'generator-star-spacing': ['error', {before: false, after: true}]
+  }
+};
---
+++ app/index.js
@@ -0,0 +1,26 @@
+const koa = require('koa')
+const kStatic = require('koa-static')
+const Pug = require('koa-pug')
+const pathHelper = require('path')
+
+const PORT = process.env.PORT || 3000
+const PUBLIC_DIR = pathHelper.resolve(__dirname, '..', 'public')
+const PRODUCTION = process.env.NODE_ENV !== 'production'
+
+const app = koa()
+const pug = new Pug({
+  viewPath: pathHelper.join(__dirname, 'views'),
+  debug: PRODUCTION,
+  pretty: PRODUCTION,
+  compileDebug: PRODUCTION
+})
+
+app.use(pug.middleware)
+
+app.use(kStatic(PUBLIC_DIR))
+
+app.use(function* () {
+  this.render('holding.pug')
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
