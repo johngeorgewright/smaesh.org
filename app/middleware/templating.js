9b37c1d282d4838a83d62aee2dd759797ea8f3c5
John Wright <johngeorge.wright@gmail.com>
2016-08-25 15:06:32 +0100

    Using fonts rather than images

--- app/middleware/templating.js
+++ app/middleware/templating.js
@@ -1,14 +1,14 @@
 const Pug = require('koa-pug')
 const resolvePath = require('path').resolve
-const PRODUCTION = process.env.NODE_ENV !== 'production'
+const NOT_PRODUCTION = process.env.NODE_ENV !== 'production'
 
 module.exports = app => {
   const pug = new Pug({
     viewPath: resolvePath(__dirname, '..', 'views'),
     helperPath: resolvePath(__dirname, '..', 'helpers'),
-    debug: PRODUCTION,
-    pretty: PRODUCTION,
-    compileDebug: PRODUCTION,
+    debug: NOT_PRODUCTION,
+    pretty: NOT_PRODUCTION,
+    compileDebug: NOT_PRODUCTION,
     locals: {}
   })
 
